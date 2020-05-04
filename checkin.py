281e79a7869040c7b3d54f00bf304641f23f19e5
John Moon <johncarlmoon@gmail.com>
2020-05-03 23:29:49 -0700

    Adding sendgrid support for confirmation email

--- checkin.py
+++ checkin.py
@@ -2,7 +2,7 @@
 """Southwest Checkin.
 
 Usage:
-  checkin.py CONFIRMATION_NUMBER FIRST_NAME LAST_NAME [-v | --verbose]
+  checkin.py CONFIRMATION_NUMBER FIRST_NAME LAST_NAME [CONF_EMAIL] [-v | --verbose]
   checkin.py (-h | --help)
   checkin.py --version
 
@@ -22,11 +22,16 @@ from southwest import Reservation, openflights
 from threading import Thread
 import sys
 import time
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
 
 CHECKIN_EARLY_SECONDS = 5
+ACTIVE_CHECKINS = os.path.dirname(os.path.realpath(__file__)) + "/active_checkins.csv"
+SG_API_KEY = os.environ.get('SENDGRID_API_KEY')
+SG_FROM_EMAIL = os.environ.get('SG_FROM_EMAIL')
 
-
-def schedule_checkin(flight_time, reservation):
+def schedule_checkin(flight_time, reservation, email=""):
     checkin_time = flight_time - timedelta(days=1)
     current_time = datetime.utcnow().replace(tzinfo=utc)
     # check to see if we need to sleep until 24 hours before flight
@@ -37,18 +42,42 @@ def schedule_checkin(flight_time, reservation):
         m, s = divmod(delta, 60)
         h, m = divmod(m, 60)
         print("Too early to check in.  Waiting {} hours, {} minutes, {} seconds".format(trunc(h), trunc(m), s))
+        # CSV line representing this reservation
+        csv = reservation_number + "," + first_name + "," + last_name + "," + email + "," + str(flight_time) + "\n"
+
+        # Since we're waiting for this checking, add ourselves to a list of active checkins
+        # allowing us to get reset if this thread dies for some reason
+        with open(ACTIVE_CHECKINS, "w+") as f:
+            for line in f:
+                if line == csv:
+                    break
+            else:
+                f.write(csv);
         try:
             time.sleep(delta)
         except OverflowError:
             print("System unable to sleep for that long, try checking in closer to your departure date")
             sys.exit(1)
+
     data = reservation.checkin()
     for flight in data['flights']:
         for doc in flight['passengers']:
-            print("{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']))
+            s = "{} got {}{}!".format(doc['name'], doc['boardingGroup'], doc['boardingPosition']);
+            email_body += s
 
+    # Remove from CSV file once reservation is completed
+    active_tmp =  ACTIVE_CHEKINS + '.bak'
+    with open(ACTIVE_CHECKINS, 'r') as active, open(active_tmp, 'w') as tmp:
+        for line in active:
+            if line != csv:
+                write_obj.write(line)
+    os.remove(active)
+    os.rename(tmp, active)
 
-def auto_checkin(reservation_number, first_name, last_name, verbose=False):
+    if email and email_body:
+        send_confirmation_email(email, email_body)
+
+def auto_checkin(reservation_number, first_name, last_name, email="", verbose=False):
     r = Reservation(reservation_number, first_name, last_name, verbose)
     body = r.lookup_existing_reservation()
 
@@ -67,23 +96,33 @@ def auto_checkin(reservation_number, first_name, last_name, verbose=False):
         date = airport_tz.localize(datetime.strptime(takeoff, '%Y-%m-%d %H:%M'))
         if date > now:
             # found a flight for checkin!
-            print("Flight information found, departing {} at {}".format(airport, date.strftime('%b %d %I:%M%p')))
+            print("Flight information found, departing {} at {}".format(airport, date.strftime('%b %d %I:%M%p')), flush=True)
             # Checkin with a thread
-            t = Thread(target=schedule_checkin, args=(date, r))
+            t = Thread(target=schedule_checkin, args=(date, r, email))
             t.daemon = True
             t.start()
             threads.append(t)
 
+    time.sleep(0.2)
+    print("No more flights associated with this reservation")
     # cleanup threads while handling Ctrl+C
     while True:
         if len(threads) == 0:
             break
         for t in threads:
             t.join(5)
-            if not t.isAlive():
+            if not t.is_alive():
                 threads.remove(t)
                 break
 
+def send_confirmation_email(email, email_body):
+    sg = sendgrid.SendGridAPIClient(api_key=SG_API_KEY)
+    from_email = Email(SG_FROM_EMAIL)
+    to_email = To(email)
+    subject = "You're checked in!"
+    content = Content("text/plain", email_body)
+    mail = Mail(from_email, to_email, subject, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
 
 if __name__ == '__main__':
 
@@ -91,11 +130,12 @@ if __name__ == '__main__':
     reservation_number = arguments['CONFIRMATION_NUMBER']
     first_name = arguments['FIRST_NAME']
     last_name = arguments['LAST_NAME']
+    email = arguments['CONF_EMAIL']
     verbose = arguments['--verbose']
 
     try:
         print("Attempting to check in {} {}. Confirmation: {}\n".format(first_name, last_name, reservation_number))
-        auto_checkin(reservation_number, first_name, last_name, verbose)
+        auto_checkin(reservation_number, first_name, last_name, email, verbose)
     except KeyboardInterrupt:
         print("Ctrl+C detected, canceling checkin")
         sys.exit()
---
+++ restart_active.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -o errexit
+
+# Run this script to restart any "active" checkins that might have been killed
+sw_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+active="${sw_dir}/active_checkins.csv"
+checkin="${sw_dir}/checkin.py"
+
+# If there's no file, just exit
+if [ ! -f "$active" ]; then
+	exit 0
+fi
+
+# Read all active checkins and start them if they don't exist
+while read -r line; do
+	conf_n=$(printf "%s" "$line" | cut -d ',' -f 1)
+	f_name=$(printf "%s" "$line" | cut -d ',' -f 2)
+	l_name=$(printf "%s" "$line" | cut -d ',' -f 3)
+	email=$(printf "%s" "$line" | cut -d ',' -f 4)
+
+	# If no process exists with this confirmation number, skip it
+	if ! pgrep -f "$conf_n" > /dev/null 2>&1; then
+		nohup python -u "$checkin" \
+			"$conf_n" "$f_name" "$l_name" "$email" > /dev/null 2>&1 &
+	fi
+done < <(cut -d ',' -f 1-4 "$active" | uniq)
